18c7d12a70aa6adace4d38be79981966dc7e0d34
Paulus Pärssinen <paulus.parssinen01@edupori.fi>
2018-12-18 22:21:21 +0200

    Implement IDisposable + minor clean-up.

--- MiControl/MiController.cs
+++ MiControl/MiController.cs
@@ -1,38 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net;
-using System.Net.Sockets;
-using System.Threading.Tasks;
 using System.Linq;
 using System.Drawing;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace MiControl
 {
-    public class MiController
+    public class MiController : IDisposable
     {
+        private static Random _rand = new Random();
+
         public IPEndPoint Endpoint { get; private set; }
+
         public bool Connected { get; private set; }
         public bool Open { get; set; }
+
         public byte BridgeId { get; private set; }
         public byte Zone { get; private set; }
 
         private readonly Socket _client;
-        private static Random _randomizer = new Random();
-
-        private const byte _filler = 0x00;
-
         private int _sequentialByte = 0x01;
 
-        private byte[] _getPrefixBytes(byte id)
-            => new byte[] { 0x80, _filler, _filler, _filler, 0x11, id, _filler, _filler, (byte)(++_sequentialByte & 0xFF), _filler };
-
-        public MiController(string ip, byte zone = 1, int port = 5987)
+        public MiController(string ip, int port = 5987, byte zone = 1)
+            : this(new IPEndPoint(IPAddress.Parse(ip), port))
+        { }
+        public MiController(IPEndPoint endpoint, byte zone = 1)
         {
-            Endpoint = new IPEndPoint(IPAddress.Parse(ip), port);
             _client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            Connected = false;
-            Open = true;
+
+            Endpoint = endpoint;
             Zone = zone;
+            Open = true;
         }
 
         public bool Connect()
@@ -45,10 +44,9 @@ namespace MiControl
                 byte[] data = Receive();
                 BridgeId = data[19];
             }
-
-            Connected = _client.Connected;
-            Task.Factory.StartNew(KeepAlive);
-            return Connected;
+            
+            Task.Factory.StartNew(KeepAliveAsync);
+            return Connected = _client.Connected;
         }
 
         public void Close()
@@ -59,11 +57,11 @@ namespace MiControl
             _client.Close();
         }
 
-        public byte[] Receive()
+        public byte[] Receive(int bufferSize = 128)
         {
-            byte[] responseBuffer = new byte[128];
-            int bytesAvailable = _client.Receive(responseBuffer);
-            return responseBuffer;
+            byte[] buffer = new byte[bufferSize];
+            int bytesAvailable = _client.Receive(buffer);
+            return buffer;
         }
 
         public void SendRaw(byte[] rawCommand)
@@ -73,16 +71,16 @@ namespace MiControl
 
         public void SendCommand(byte[] command)
         {
-            byte[] prefix = _getPrefixBytes(BridgeId);
-            byte[] toSum = MiHelpers.Combine(command, new byte[] { Zone, _filler });
+            byte[] prefix = GetCommandPrefix(BridgeId);
+            byte[] toSum = MiHelpers.Combine(command, new byte[] { Zone, 0x00 });
             byte[] cmd = MiHelpers.Combine(prefix, toSum, new byte[] { (byte)toSum.Sum(x => x) });
 
             SendRaw(cmd);
         }
 
-        private async void KeepAlive()
+        private async Task KeepAliveAsync()
         {
-            byte[] keepAliveCommand = new byte[] { 0xD0, _filler, _filler, _filler, 0x02, BridgeId, _filler };
+            byte[] keepAliveCommand = new byte[] { 0xD0, 0x00, 0x00, 0x00, 0x02, BridgeId, 0x00 };
 
             while (Connected)
             {
@@ -93,6 +91,9 @@ namespace MiControl
             if (Open)
                 Connect(); // try to re-connect, but this might be hopeless
         }
+
+        private byte[] GetCommandPrefix(byte id)
+            => new byte[] { 0x80, 0x00, 0x00, 0x00, 0x11, id, 0x00, 0x00, (byte)(++_sequentialByte & 0xFF), 0x00 };
 
         public void TurnOn()
             => SendCommand(MiCommands.TurnOn);
@@ -114,5 +115,26 @@ namespace MiControl
 
         public void SetPartyMode(MiMode mode)
             => SendCommand(MiCommands.SetPartyMode(mode));
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (Connected)
+                {
+                    try
+                    {
+                        _client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.DontLinger, true);
+                        _client.Shutdown(SocketShutdown.Both);
+                    }
+                    catch { }
+                }
+                _client.Close();
+            }
+        }
     }
 }
